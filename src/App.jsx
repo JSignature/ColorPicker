d2d53c2a778f36877b6061c50555076ac0ca9400
Jimmy Signature <jimmysignature@gmail.com>
2023-07-02 21:28:50 -0400

    add onClick event

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 // Write your Color component here
-import Color from './Color'
+import { useState } from 'react'
+
+const Color = props => {
+  return (
+    <div
+      className={props.color}
+      onClick={() => props.selected(props.color)}
+    ></div>
+  )
+}
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState('')
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
         {
           <div id="colors-list">
-            <Color color="blue" />
-            <Color color="red" />
-            <Color color="green" />
+            <Color color="blue" selected={setSelectedColor} />
+            <Color color="red" selected={setSelectedColor} />
+            <Color color="green" selected={setSelectedColor} />
           </div>
         }
       </div>
--- src/Color.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Color = props => {
-  console.log(props)
-  return <div className={props.color}></div>
-}
-
-export default Color
